4920761dd38ab2ca2ce8ab8de4d5872850bfb776
hyunseung.choi <75869478+dataking820@users.noreply.github.com>
2023-11-24 15:56:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -11,10 +11,17 @@ from langchain.chat_models import ChatOpenAI
 import streamlit as st
 import tempfile
 import os
+from streamlit_extras.buy_me_a_coffee import button
+
+button(username='gustmd820', floating=True, width=221)
+
 
 # 제목
 st.title("ChatPDF")
 st.write("---")
+
+# OpenAI KEY 입력 받기
+openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
 
 # 파일 업로드 
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요!", type=['pdf'])
@@ -45,7 +52,7 @@ if uploaded_file is not None:
 
     # Embedding
     from langchain.embeddings import OpenAIEmbeddings
-    embeddings_model = OpenAIEmbeddings()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
@@ -56,7 +63,7 @@ if uploaded_file is not None:
 
     if st.button('질문하기'):
         with st.spinner("wait for it..."):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key)
             qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
             result = qa_chain({"query": question})
             st.write(result["result"])
