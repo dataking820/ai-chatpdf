437ad1ac1d840429fba1dcb6c642a69bfd344a8d
hyunseung.choi <75869478+dataking820@users.noreply.github.com>
2023-11-24 17:37:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -12,6 +12,7 @@ import streamlit as st
 import tempfile
 import os
 from streamlit_extras.buy_me_a_coffee import button
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 button(username='gustmd820', floating=True, width=221)
 
@@ -57,13 +58,25 @@ if uploaded_file is not None:
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
 
+    # Stream 받아 줄 Handler 만들기 
+    from langchain.callbacks.base import BaseCallbackHandler
+    class StreamHandler(BaseCallbackHandler):
+        def __init__(self, container, initial_text=""):
+            self.container = container
+            self.text=initial_text
+        def on_llm_new_token(self, token: str, **kwargs) -> None: 
+            self.text+=token
+            self.container.markdown(self.text)
+
     # Question
     st.header("PDF에게 질문해보세요!!!")
     question = st.text_input('질문을 입력하세요')
 
     if st.button('질문하기'):
         with st.spinner("wait for it..."):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key)
+            chat_box = st.empty()
+            stream_handler = StreamHandler(chat_box)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key, streaming=True, callbacks=[stream_handler])
             qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
-            result = qa_chain({"query": question})
-            st.write(result["result"])
+            qa_chain({"query": question})
+            
